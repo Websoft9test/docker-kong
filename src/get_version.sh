5a0937a0071960bc7ac822561d3b02baefd870b9
qiaofeng1227 <76487013@qq.com>
2022-07-22 15:24:35 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo kong version: $(docker exec -it $1 bash -c "kong version") |sudo tee -a /data/logs/install_version.txt
+sudo echo kong version: $(docker exec -i $1 kong version) |sudo tee -a /data/logs/install_version.txt
